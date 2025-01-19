d3d6baaddd113bf567fc6912361541958720665a
hdz-kevin <kevinhdez592@gmail.com>
2025-01-19 17:26:53 -0600

    Update welcome page message

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (isset($_SESSION["user"])) {
 
 <div class="welcome d-flex align-items-center justify-content-center">
   <div class="text-center">
-    <h1>Store Your Contacts Now</h1>
+    <h1>Store Your Contacts Now!</h1>
     <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
   </div>
 </div>
